c0ca7b1b8e01495ba9bae67c3c52a077c41f9f86
Raj-venom <rajsingh.rsr9824@gmail.com>
2024-06-10 16:09:11 +0545

    websocket initialized

---
+++ WebSocket/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import http from "http"
+import { Server } from "socket.io";
+
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+
+const PORT = 4000
+
+app.use(express.static('public'))
+
+io.on("connection", (Socket) =>{
+    console.log("A user connected", Socket.id);
+
+    Socket.on("message", (msg) => {
+        console.log("client message",msg);
+       io.emit("message", msg)
+    })
+})
+
+// io.on('connection', (socket) => {
+//     console.log('a user connected');
+//     socket.on('disconnect', () => {
+//       console.log('user disconnected');
+//     });
+//   });
+
+server.listen(PORT, () => {
+    console.log("server running on port:", PORT)
+})
