676c03e3057918d14f5da01b94e3dff8a2d5e22e
Raj-venom <rajsingh.rsr9824@gmail.com>
2024-06-11 15:32:00 +0545

    Add healthcheck routes

--- Otp Authentication/Backend/src/routes/user.routes.js
+++ Otp Authentication/Backend/src/routes/user.routes.js
@@ -8,6 +8,7 @@ import {
 } from "../controllers/user.controller.js"
 
 import { verifyJWT } from "../middlewares/auth.middleware.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const router = Router()
 
@@ -16,5 +17,9 @@ router.route("/login").post(loginUser)
 router.route("/current-user").get(verifyJWT, getCurrentUser)
 router.route("/refresh-token").post(refreshAccessToken)
 router.route("/verify-otp").post(verifyOtp)
+router.route("/health").get((req,res) => {
+    res.status(200)
+    .json(new ApiResponse(200, {} , "ok"))
+})
 
 export default router
