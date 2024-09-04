6c5ebe911ba75997c662c365bdff747a8221d795
Raj-venom <rajsingh.rsr9824@gmail.com>
2024-09-05 00:33:15 +0545

    chore: Add Redis caching for API response

--- redis playground/index.js
+++ redis playground/index.js
@@ -1,1 +1,32 @@
+import express from "express";
+import axios from "axios";
+import { client } from "./client.js";
+
+
+const app = express();
+
+
+app.get("/", async (req, res) => {
+
+    // stroing in redis server
+    const catchedData = await client.get("posts")
+
+    if(catchedData){
+        console.log("Data is cached")
+        return res.json(JSON.parse(catchedData))
+    }
+
+    const {data} = await axios.get("https://jsonplaceholder.typicode.com/posts")
+    await client.set("posts", JSON.stringify(data))
+    await client.expire("posts", 30)
+
+    console.log("i am here")
+    return res.json(data)
+
+})
+
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
+})
 
